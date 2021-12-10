d6002222990a2a6855e3b2aa70223f259f53bd16
Charles Blain <74670182+CharlyMittsu@users.noreply.github.com>
2021-12-10 09:38:34 +0100

    jaune

--- motus.py
+++ motus.py
@@ -4,22 +4,51 @@ from colorama import Fore, Back, Style
 #print(Fore.RED + 'some red text', end=" ")
 
 import random
-
-masque=""
+end = False
+essais=0
 
 fichierSource = open("liste.txt","r")
 tableau = fichierSource.readlines()
 
-motMystere = tableau[random.randint (0,len(tableau))]
+motMystere = tableau[random.randint (0,len(tableau))-1]
 
-def masque() :
-    masque=""
-    for i in range (len(motMystere)):
-        masque=masque+"*"
-    return masque
+def red(mot,lettre):
+    if lettre==mot:
+        return True
+    else:
+        return False
 
+def yellow(mot,lettre):
+    compte=0
+    for i in range(6):
+        if mot[i]==lettre:
+            compte+=1
+    return compte
+
+
+        
 #jeu
 
-masque=masque()
-print (masque)
 print(motMystere)
+while end == False:
+    guess=""
+    while len(guess)!=6:
+        guess = input()
+        if len(guess)!=6:
+            print(Fore.WHITE + "Le mot doit faire 6 lettre.")
+    good = 0
+    for i in range (6):
+        if red(motMystere[i],guess[i])==True:
+            print(Fore.RED + guess[i], end="")
+            good= good+1
+        elif:
+            same=yellow(motMystere,guess[i])
+            if same>0:
+                print(Fore.YELLOW + guess[i], end="")
+        else:
+            print(Fore.BLUE + guess[i], end="")
+    print(Fore.WHITE + "")
+    essais+=1
+    if good ==6:
+        end=True
+print(Fore.GREEN + "gg vous avez trouvé en",essais,"éssais")
