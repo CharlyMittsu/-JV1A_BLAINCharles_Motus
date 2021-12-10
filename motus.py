66ecfc89fc770d221fd917ffdb65ca5760526835
Charles Blain <74670182+CharlyMittsu@users.noreply.github.com>
2021-12-10 08:35:16 +0100

    masque

--- motus.py
+++ motus.py
@@ -5,5 +5,21 @@ from colorama import Fore, Back, Style
 
 import random
 
+masque=""
+
 fichierSource = open("liste.txt","r")
 tableau = fichierSource.readlines()
+
+motMystere = tableau[random.randint (0,len(tableau))]
+
+def masque() :
+    masque=""
+    for i in range (len(motMystere)):
+        masque=masque+"*"
+    return masque
+
+#jeu
+
+masque=masque()
+print (masque)
+print(motMystere)
